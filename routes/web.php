43cc33d0e3f5b1cfa054e0218d1218beb04f2b49
Luchy2448 <130091006+Luchy2448@users.noreply.github.com>
2025-04-24 21:41:13 -0300

    Layouts, Admin panel, Admin Middleware created.

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function () {
+   // login
+   Route::match(['get', 'post'], '/login', [AdminController::class, 'login'])->name('admin.login');
+   
+   Route::group(['middleware' => ['admin']], function () {
+    Route::get('dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+   }); 
+});
