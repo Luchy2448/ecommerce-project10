2710c4629dd1a565477a8bed1cde0f3e6857d8d3
Luchy2448 <130091006+Luchy2448@users.noreply.github.com>
2025-05-22 17:50:27 -0300

    Admin panel finished

--- resources/views/admin/layout/footer.blade.php
+++ resources/views/admin/layout/footer.blade.php
@@ -1,7 +1,8 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; <?php echo date('Y'); ?> <a href="https://adminlte.io">Giga<b
+                style="color:lime;">S</b>tore</a>.</strong>
     All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
+    {{-- <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 3.2.0
-    </div>
+    </div> --}}
 </footer>
